d708465403fa551873095f5a114edfe48fc94040
Yakov Vorontsov <wlbmJames@gmail.com>
2019-11-29 12:18:00 +0700

    Image anapyzer project

---
+++ ImageAnalyzer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using ATAPY.Common.IO;
+using ATAPY.Document.Data.Core;
+using TextExtractor;
+using DataAnalyzer;
+using System.Collections.Generic;
+
+namespace ImageAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+                 throw new Exception("");
+            var pathToImage = args[0];
+            var pathToResultXml = args[1];
+            var document = GetDoc(pathToImage);
+            var dClassifier = new Classifier();
+            dClassifier.AddClass(DocClassesCollector.Tawunia());
+            dClassifier.AddClass(DocClassesCollector.Bupa());
+            var docs = dClassifier.Classify(document);
+            var analyzer = new Analyzer();
+            var models = new List<ResultModel>();
+            foreach (var doc in docs)
+            {
+                analyzer.AnalyzeDocument(doc);
+                var model = ResultHandler.GetResultModel(doc);
+                models.Add(model);
+            }
+            ResultHandler.SaveModel(models, pathToResultXml);
+        }
+        private static Document GetDoc(string pathToImage)
+        {
+            var extractor = new Extractor();
+            var file = new File(pathToImage);
+            var doc = extractor.GetDocument(file);
+            return doc;
+        }
+    }
+}
